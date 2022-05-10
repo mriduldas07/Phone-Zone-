982c0c3977e403bc5f0874040d077dc6c08b972e
mriduldas07 <mriduldas0325@gmail.com>
2022-05-10 15:59:26 +0600

    delivered btn

--- src/components/InventoryIdItem/InventoryIdItem.js
+++ src/components/InventoryIdItem/InventoryIdItem.js
@@ -37,6 +37,25 @@ const InventoryIdItem = () => {
                 setIsReload(!isReload)
             });
     };
+
+    //  set delivered button to decrease one by one
+    const handleDeliveredBtn = () =>{
+        const quantity = parseInt(phone.quantity) - 1;
+        const updatedPhoneQuantity = { quantity };
+        //PUT data to server
+        const url = `http://localhost:5000/phones/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(updatedPhoneQuantity),
+        })
+        .then(res => res.json())
+        .then(data => {
+            setIsReload(!isReload);
+        })
+    }
     return (
         <>
         <h2 className='text-center pt-3'>Details About {name}</h2>
@@ -51,7 +70,7 @@ const InventoryIdItem = () => {
                         <h6>Quantity: {quantity}</h6>
                         <h6>Supplier Name: {supplier}</h6>
                     </Card.Text>
-                <Button variant="success">Delivered</Button>
+                <Button variant="success" onClick={() => handleDeliveredBtn()}>Delivered</Button>
             </Card.Body>
         </Card>
         <form onSubmit={handleUpdateForm} className='login-form mx-auto w-25 my-3 py-5'>
